de8a9d78856eeceb4bf8d58e1ade4fd166e15c1f
salva <s@salva.io>
2018-11-28 01:19:39 -0300

    read qr

--- App.js
+++ App.js
@@ -7,58 +7,68 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import { CameraKitCamera } from 'react-native-camera-kit';
+import {Platform, StyleSheet, Text, View, PermissionsAndroid} from 'react-native';
+import { CameraKitCameraScreen } from 'react-native-camera-kit';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component {
+  state = {
+    granted: false
+  }
+  componentWillMount() {
+    const grant = () => {
+      this.setState({granted: true})
+    }
+    async function requestCameraPermission() {
+      if (Platform.OS === 'android') {
+        try {
+          const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.CAMERA,
+            PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+            PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+            {
+              'title': 'Cool Photo App Camera Permission',
+              'message': 'Cool Photo App needs access to your camera ' +
+                        'so you can take awesome pictures.'
+            }
+          );
+          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            grant();
+          } else {
+            console.warn("Camera permission denied")
+          }
+  
+        } catch (err) {
+          console.warn(err)
+        }
+      }
+    }
 
-type Props = {};
-export default class App extends Component<Props> {
+    requestCameraPermission();
+  }
   render() {
     return (
-      <View style={styles.container}>
+      <View>
+        {
+          this.state.granted ?
+          <CameraKitCameraScreen
+              actions={{ rightButtonText: 'Done', leftButtonText: 'Cancel' }}
+              onBottomButtonPressed={(event) => this.onBottomButtonPressed(event)}
+              scanBarcode={true}
+              laserColor={"blue"}
+              frameColor={"yellow"}
 
-        <CameraKitCamera
-          ref={cam => this.camera = cam}
-          style={{
-            flex: 1,
-            backgroundColor: 'white'
-          }}
-          cameraOptions={{
-            flashMode: 'auto',             // on/off/auto(default)
-            focusMode: 'on',               // off/on(default)
-            zoomMode: 'on',                // off/on(default)
-            ratioOverlay:'1:1',            // optional, ratio overlay on the camera and crop the image seamlessly
-            ratioOverlayColor: '#00000077' // optional
-          }}
-          onReadQRCode={(event) => console.log(event.nativeEvent.qrcodeStringValue)} // optional        
-        />
+              onReadQRCode={((event) => Alert.alert("Qr code found"))} //optional
+              hideControls={false}           //(default false) optional, hide buttons and additional controls on top and bottom of screen
+              showFrame={true}   //(default false) optional, show frame with transparent layer (qr code or barcode will be read on this area ONLY), start animation for scanner,that stoped when find any code. Frame always at center of the screen
+              offsetForScannerFrame = {10}   //(default 30) optional, offset from left and right side of the screen
+              heightForScannerFrame = {300}  //(default 200) optional, change height of the scanner frame
+              colorForScannerFrame = {'red'} //(default white) optional, change colot of the scanner frame
+          />
+          :
+          <Text>error de permisos</Text>
+        }
 
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
